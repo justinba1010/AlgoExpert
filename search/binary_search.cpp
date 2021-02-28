44135a4b59c49ce51f3166f28c7d8777d361c45e
Justin Baum <justin.baum@ninefx.com>
2021-02-28 18:27:42 -0500

    Add two easy searches

---
+++ search/binary_search.cpp
@@ -0,0 +1,14 @@
+int binarySearch(vector<int> arr, int target) {
+  // Write your code here.
+	int i = 0;
+	int j = arr.size();
+	int x;
+	
+	while (i != j) {
+		x = ((j - i) >> 1) + i;
+		if (arr[x] == target) return x;
+		if (arr[x] < target) i = x + 1;
+		else j = x;
+	}
+  return -1;
+}
