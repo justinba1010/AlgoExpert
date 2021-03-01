6285c64876892ee321b312aa0336ff8daf653896
Justin Baum <justin.baum@ninefx.com>
2021-02-28 21:23:22 -0500

    Add min jumps; this was fun

---
+++ dynamic_prog/min_num_jumps.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+int minNumberOfJumps(vector<int> array) {
+    int size = array.size();
+    vector<int> jumps = vector<int>(size, INT_MAX >> 1);
+    jumps[size-1] = 0;
+    for (int i = size - 2; i >= 0; --i) {
+        int forward = min(array[i], size - i);
+        for (int j = 1; j <= forward; ++j) {
+            jumps[i] = min(jumps[i], 1 + jumps[i + j]);
+        }
+    }
+    return jumps[0];
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # Algorithm Practice in C++
 
 Using AlgoExpert to brush up on C++
+
+
+To remove tabs
+`find . -iname '*.cpp' -type f -exec sed -i.orig 's/\t/    /g' {} +`
