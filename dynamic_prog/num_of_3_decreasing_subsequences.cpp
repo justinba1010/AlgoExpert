2588329263388d9f669c600fa494689aa4da9c95
Justin Baum <justin.baum@ninefx.com>
2021-03-08 19:03:50 -0500

    3 decreasing subsequences

---
+++ dynamic_prog/num_of_3_decreasing_subsequences.cpp
@@ -0,0 +1,33 @@
+/* Copyright 2021
+** Justin Baum
+** MIT License
+** AlgoExpert Solutions
+*/
+
+long maxInversions(vector<int> arr) {
+    // Dynamic
+    // Naive is O(n^3)
+    // Optimal must be O(n^2) as this is doable with memoization
+    int n = arr.size();
+    if (n == 0) return 0;
+    // dynamic[i][j] = max(dynamic[i-1][j], )
+    vector<int> lefts = vector<int>(n, 0);
+    vector<int> rights = vector<int>(n, 0);
+    for (int i = 1; i < n - 1; ++i) {
+        for (int j = i - 1; j >= 0; --j) {
+            lefts[i] += (arr[j] > arr[i]);
+        }
+        for (int j = i + 1; j < n; ++j) {
+            rights[i] += (arr[i] > arr[j]);
+        }
+    }
+    for (auto x : lefts) cout << x << " ";
+    cout << endl;
+    for (auto x : rights) cout << x << " ";
+    cout << endl;
+    long count = 0;
+    for (int i = 1; i < n - 1; ++i) {
+        // This is the number of combos possible
+        count += lefts[i] * rights[i]; 
+    }
+    return count;
